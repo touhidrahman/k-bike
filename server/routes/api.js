5d442b02002a106644d5fb1a28ab9a1a1610274b
Touhidur Rahman <touhidrahman1987@gmail.com>
2019-03-05 22:27:43 +0600

    :sparkles: Add new routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,6 +8,93 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+router.get('/bikes', (req, res) => {
+  Bike.find((err, results) => {
+    if (err) res.json(err);
+
+    res.json(results);
+  })
+})
+
+router.post('/bike/new', (req, res) => {
+  Bike.create({
+    username: '',
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    rented: false,
+  }, (err, savedBike) => {
+    if (err) res.json(err);
+
+    res.json(savedBike);
+  });
+});
+
+router.get('/rent-bike/:id', (req, res) => {
+  const id = req.params.id;
+
+  Bike.findById(id, (err, bike) => {
+    if (err) res.json(err);
+
+    // if not rented, only then rent
+    if (bike && !bike.rented) {
+      const toSave = {
+        username: req.body.username,
+        rented: true
+      }
+
+      Bike.findByIdAndUpdate(id, toSave, (err, savedBike) => {
+        if (err) res.json(err);
+
+        // TODO: sending old value instead of changed one
+        res.json(savedBike);
+      })
+    } else {
+      // send error message
+      res.end();
+    }
+  });
+});
+
+router.get('/return-bike/:id', (req, res) => {
+  const id = req.params.id;
+
+  Bike.findById(id, (err, bike) => {
+    if (err) res.json(err);
+
+    // if rented, only then return
+    if (bike && bike.rented) {
+      const toSave = {
+        username: req.body.username,
+        rented: false
+      }
+
+      Bike.findByIdAndUpdate(id, toSave, (err, savedBike) => {
+        if (err) res.json(err);
+
+        res.json(savedBike);
+      })
+    } else {
+      // send error message
+    }
+  });
+});
+
+router.put('/bike/:id', (req, res) => {
+  const id = req.params.id;
+  const toSave = {
+    username: req.body.username,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    rented: req.body.rented
+  }
+
+  Bike.findByIdAndUpdate(id, toSave, (err, savedBike) => {
+    if (err) res.json(err);
+
+    res.json(savedBike);
+  });
+});
+
 router.post('/user/new', (req, res) => {
   User.create({
     name: req.body.name,
