32ad3bcac3e40737833383888ae6108f424292dd
Touhidur Rahman <touhidrahman1987@gmail.com>
2019-03-07 19:42:33 +0600

    :sparkles: Add leaflet map in frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tileLayer, latLng, circle, polygon } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'k-bike';
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 10,
+    center: latLng(51.879966, 8.726909)
+  };
+
+  layersControl = {
+    baseLayers: {
+        'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+        'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    },
+    overlays: {
+        'Big Circle': circle([ 46.95, -122 ], { radius: 5000 }),
+        'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
 }
