62853af6fb7f49d985659f6fff124d187ec940f7
Touhid Rahman <touhidrahman1987@gmail.com>
2019-03-17 06:41:36 +0100

    :lipstick: Get map size dynamically

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Bike } from '../app.types';
 import { BikeService } from '../bike.service';
 import { Subject, Subscription } from 'rxjs';
@@ -12,20 +12,42 @@ import { AuthService } from '../auth/auth.service';
 export class MapComponent implements OnInit, OnDestroy {
 
   bikes: Array<Bike>;
-  rentedBike: Bike;
   userId: string;
-  bikesSub: Subscription;
+  bikes$: Subscription;
+
+  screenHeight: number;
+  screenWidth: number;
+  mapCenterLat: number;
+  mapCenterLong: number;
 
   constructor(
     private bikeService: BikeService,
     private authService: AuthService,
-  ) { }
+  ) {
+    // to set map size
+    this.getScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+  }
 
   ngOnInit(): void {
     this.bikeService.getBikes();
 
-    this.bikesSub = this.bikeService.getBikesUpdatedListener().subscribe(value => {
+    this.bikes$ = this.bikeService.getBikesUpdatedListener().subscribe(value => {
       this.bikes = value.bikes;
+
+      let latSum = 0;
+      let longSum = 0;
+      this.bikes.forEach(it => {
+        latSum += it.latitude;
+        longSum += it.longitude;
+      });
+      this.mapCenterLat = latSum / this.bikes.length;
+      this.mapCenterLong = longSum / this.bikes.length;
     });
 
     this.userId = this.authService.getLoggedInUserId();
@@ -40,6 +62,6 @@ export class MapComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.bikesSub.unsubscribe();
+    this.bikes$.unsubscribe();
   }
 }
